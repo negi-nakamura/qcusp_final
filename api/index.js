7b24eacb5e62c2a654610c644707c3c0dcee1862
unknown <regaladochristian999.pro@gmail.com>
2026-05-04 02:41:20 +0800

    ho

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://qcustudentportal.vercel.app/",
+    origin: "https://qcustudentportal.vercel.app",
     credentials: true
   })
 );
