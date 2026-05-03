5e70eb9cbdfa2de49e7f0fdd4881efa8ba65a152
unknown <regaladochristian999.pro@gmail.com>
2026-05-04 01:31:35 +0800

    hello

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import v1Routes from "./src/api/v1/routes.js";
+import v1Routes from "../src/api/v1/routes.js";
 
 const app = express();
 
